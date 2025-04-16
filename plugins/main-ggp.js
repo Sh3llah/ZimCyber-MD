170c18db847c653ca0130b1d6b6d4a20ad5257fc
Sh3llah <sh3llahcyber7@gmail.com>
2025-04-16 10:18:08 +0200

    Add files via upload

---
+++ plugins/main-ggp.js
@@ -0,0 +1,15 @@
+let handler = async m => m.reply(`𝚂𝚄𝙿𝙿𝙾®𝚃 𝙶𝚁𝙾𝚄𝙿 𝙻𝙸𝙽𝙺
+cyber
+
+*${mssg.link}*: https://whatsapp.com/channel/0029Vb5SP7IDjiOfSjLKlB2Y
+
+*${mssg.link}*: https://whatsapp.com/channel/0029VaKNbWkKbYMLb61S1v11
+
+*OWNER*
+${developer}
+`.trim())
+handler.help = ['support']
+handler.tags = ['main']
+handler.command = ['support', 'sup'] 
+
+export default handler
